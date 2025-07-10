8e3d6631a644915a5d86c173167a64dee14653f0
ILyass <lirmaqui.ilyass@gmail.com>
2025-07-10 16:50:35 +0100

    Changed port format to be ':PortNumber'.

--- main.go
+++ main.go
@@ -63,10 +63,10 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "5000"
+		port = ":5000"
 	}
 
-	log.Fatal(app.Listen("0.0.0.0:" + port))
+	log.Fatal(app.Listen("0.0.0.0" + port))
 
 }
 func getTodos(c *fiber.Ctx) error {
