4c44ff9b06c212b85937b51b9257d230a6eccd13
ILyass <lirmaqui.ilyass@gmail.com>
2025-07-10 23:30:22 +0100

    Fixed port format

--- main.go
+++ main.go
@@ -66,14 +66,14 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = ":5000"
+		port = "5000"
 	}
 
 	if os.Getenv("ENV") == "production" {
 		app.Static("/", "./client/dist")
 	}
 
-	log.Fatal(app.Listen("0.0.0.0" + port))
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
 
 func getTodos(c *fiber.Ctx) error {
